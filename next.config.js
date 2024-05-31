1a8c5c1b932614f5a48b08df08979ea58271ae8a
Dwiprstyo <dwiprstayo@gmail.com>
2024-05-31 15:50:44 +0700

    :bug: error build to vercel

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
@@ -5,22 +6,22 @@ const nextConfig = {
         source: '/api/:path*',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/api/:path*' // Development endpoint
-            : 'http://127.0.0.1:8000/api/:path*', // Production endpoint
+            ? 'http://127.0.0.1:8000/api/:path*'
+            : '/api/:path*'
       },
       {
         source: '/docs',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/docs' // Development endpoint
-            : '/api/docs', // Production endpoint
+            ? 'http://127.0.0.1:8000/docs'
+            : '/api/docs'
       },
       {
         source: '/openapi.json',
         destination:
           process.env.NODE_ENV === 'development'
-            ? 'http://127.0.0.1:8000/openapi.json' // Development endpoint
-            : '/api/openapi.json', // Production endpoint
+            ? 'http://127.0.0.1:8000/openapi.json'
+            : '/api/openapi.json'
       },
     ];
   },
